c3270125ee41dde8038b2b2c14e206b497d64795
Samuel Kelemen <Samuel@Kelemen.us>
2020-04-22 15:46:35 -0400

    fix quote type

--- index.js
+++ index.js
@@ -8,27 +8,27 @@ API.runtime.onInstalled.addListener(function() {
                               function(newFolder) {
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '1x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 1.0)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 1.0)'});
                                 
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '1.5x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 1.5)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 1.5)'});
        
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '2x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 2.0)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 2.0)'});
         
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '2.5x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 2.5)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 2.5)'});
                                 
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '3x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 3.0)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 3.0)'});
        
                                 API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '3.5x',
-                                'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 3.5)'});
+                                'url': 'javascript:Array.from(document.getElementsByTagName("video")).map(video => video.playbackRate = 3.5)'});
         
         
         console.log("added folder: " + newFolder.title);
