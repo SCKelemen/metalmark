af762dc9a49a5a343a0eb7acf7f752ecf78a9569
Samuel Kelemen <SCKelemen@users.noreply.github.com>
2020-04-22 15:01:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-browser.runtime.onInstalled.addListener(function() {
+"use strict";
+var API = chrome || browser;
+
+API.runtime.onInstalled.addListener(function() {
     console.log("Installing Speeds");
-    browser.bookmarks.create({'parentId': browser.bookmarks.id,
+    API.bookmarks.create({'parentId': API.bookmarks.id,
                                'title': 'Playback Speed'},
                               function(newFolder) {
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '1x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 1.0)'});
                                 
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '1.5x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 1.5)'});
        
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '2x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 2.0)'});
         
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '2.5x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 2.5)'});
                                 
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '3x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 3.0)'});
        
-                                browser.bookmarks.create({'parentId': newFolder.id,
+                                API.bookmarks.create({'parentId': newFolder.id,
                                 'title': '3.5x',
                                 'url': 'javascript:Array.from(document.getElementsByTagName(‘video’)).map(video => video.playbackRate = 3.5)'});
         
